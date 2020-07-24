a92216cbc504a21c3820157a62497bfcf281a027
Aashish <Aashish@Admins-Mac.local>
2020-07-24 14:46:13 +0530

    first commit

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by Aashish on 24.07.2020.
+//  Copyright © 2020 Aashish. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Hello, Swift UI!")
+             Text("Hello, Swift UI!")        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
